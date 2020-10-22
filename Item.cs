41a47de54edb4685a778636c98472039baa7f17c
rafifauzan09 <rafif.almahdy@students.amikom.ac.id>
2020-10-23 02:57:14 +0700

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCashier
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int jumlah { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string title, int jumlah, string type, double price)
+        {
+            this.id = id;
+            this.title = title;
+            this.jumlah = jumlah;
+            this.price = price;
+            this.subtotal = 0;
+            this.type = type;
+        }
+
+        public string getTtitle()
+        {
+            return title;
+        }
+
+        public int getJumlah()
+        {
+            return jumlah;
+        }
+
+        public string getType()
+        {
+            return type;
+        }
+
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public double getSubtotal()
+        {
+            subtotal = price * jumlah;
+            return subtotal;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TheCashier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+            ListBox.ItemsSource = calculator.getListItem();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string title = ItemNameBox.Text;
+            int quantity = int.Parse(QuantityBox.Text);
+            string type = TypeBox.Text;
+            double price = double.Parse(PriceBox.Text);
+
+            Item item = new Item(new Random().Next(), title, quantity, type, price);
+            calculator.AddItem(item);
+            double total = calculator.getTotal();
+
+            TotalLabel.Content = String.Format("Rp. {0}", total);
+            ListBox.Items.Refresh();
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
